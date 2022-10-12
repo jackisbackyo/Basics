7916a55a3df14f851b6f418317808320b0d64291
jackisbackyo <114708531+jackisbackyo@users.noreply.github.com>
2022-10-11 21:26:10 -0400

    Update weight.py

--- weight.py
+++ weight.py
@@ -1,6 +1,7 @@
+print("This program converts lbs to kg and kg to lbs.")
 weight = int(input("Enter your weight: "))
-unit = input("(L)bs or (K)g: ")
-if unit.lower() == 'l':
+unit = input("(L)bs or (K)g: ").lower()
+if unit == 'l':
     converted = weight * 0.45
     print(f'You are {converted} kilos')
 else:
